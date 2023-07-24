3bc334cd0dc447d2fbc9d2fc3bf60268137a7673
uday-7799 <123895812+uday-7799@users.noreply.github.com>
2023-07-25 01:25:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,8 @@
 def power(num,exponent):
     return num**exponent
 
-power(2,3)
-
-<<<<<<< HEAD
-def underroot(num):
-    return num*(1/2)
-=======
 def sqroot(num):
     return (num)**1/2
->>>>>>> 631bc1b0fe5acb1905314645a0a26536809bb196
 
 def multiply(num1,num2):
     return num1*num2
